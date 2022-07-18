5e976f6f4b5aa076e9e723d0b6da03dbdb3bddc1
Leidiane Aguiar <leiidiane.aguiar@gmail.com>
2022-07-18 18:22:58 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/leidy/gamestoplay/GamesToPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leidy.gamestoplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesToPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
